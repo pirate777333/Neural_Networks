5c5763fa2ad9bb4de6d5df445a14e11682c44186
pirate777333 <56797544+pirate777333@users.noreply.github.com>
2020-09-21 15:24:27 +0200

    Add files via upload

---
+++ colors_6_sc_4alghoritms.py
@@ -0,0 +1,69 @@
+# IMPORT LIBRARIES
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble  import RandomForestClassifier 
+
+# IMPORT DATA
+df=pd.read_csv('C:/Users/Josko/Desktop/my_projects/Mach_L/NN/neural_network/six_colors/color_6.csv')
+df=df.sample(frac=1)
+print(df.head())
+
+X=df.iloc[:,:-1]
+y=df.iloc[:,-1]
+
+# SPLIT THE DATA TRAINING/TESTING
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+# KN CLASSIFIER
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+print('KN CLASSIFIER')
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+
+# SVM CLASSIFIER
+classifier = SVC(kernel='linear')
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+print('SVM CLASSIFIER')
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+
+# DECISION TREE CLASSIFIER
+classifier = DecisionTreeClassifier()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+print('DECISION TREE CLASSIFIER')
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+
+# RANDOM FOREST CLASSIFIER
+classifier = RandomForestClassifier() # n_estimators 
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+print('RANDOM FOREST CLASSIFIER')
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
